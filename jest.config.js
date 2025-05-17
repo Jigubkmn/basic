8d05f25ba2b55d84176f984bfd2b0968172a56ed
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-17 19:54:49 +0900

    テストコード実装

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+};
+
+export default config;
