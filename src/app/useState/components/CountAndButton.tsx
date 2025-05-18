729a9eb43f23d6bd58298ef5b7318cb6deea72cd
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-18 13:53:17 +0900

    パス名取得

--- src/app/useState/components/CountAndButton.tsx
+++ src/app/useState/components/CountAndButton.tsx
@@ -1,9 +1,14 @@
 "use client"
 
 import { useState } from "react"
+// import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+
 
 export function CountAndButton() {
   const [count, setCount] = useState(0);
+  const pathname = usePathname();
+  console.log("ルート名:", pathname);
 
   function handleClick() {
     setCount(prev => prev + 1);
